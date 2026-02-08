930127237209a451c1c6d5c366733444337b1eee
Agweelurr <jimbobobjoe@proton.me>
2026-02-08 09:50:02 -0800

    canIDs make sense, new subsystems that should have been there in the first place

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -38,7 +38,7 @@ public class Shooter extends SubsystemBase {
 
   // Constants
   private final DCMotor dcMotor = DCMotor.getKrakenX60(1);
-  private final int canID = 16;
+  private final int canID = 17;
   private final double gearRatio = 1;
   private final double kP = .55; //started at 1
   private final double kI = 0;
